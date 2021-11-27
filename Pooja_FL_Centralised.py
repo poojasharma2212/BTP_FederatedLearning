23e667aac6c73be3baae88778bc67c5df658c8b8
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2021-11-28 04:31:05 +0530

    Update Pooja_FL_Centralised.py

--- Pooja_FL_Centralised.py
+++ Pooja_FL_Centralised.py
@@ -136,7 +136,7 @@ class CNN(nn.Module):
   #   out = self.dense_layers(out)
   #   return out
   def forward(self, x):
-        x = self.conv2(x)
+        x = self.conv_layers(x)
         x = Func.max_pool2d(x,2)
         x = x.view(-1, 64*12*12)
         x = self.fc(x)
