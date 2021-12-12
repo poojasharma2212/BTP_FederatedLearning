45d77cf05f89d75f40d9b11341e6753c10d2e4f7
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2021-12-12 19:21:57 +0530

    Update Pooja_FL_Centralised.py

--- Pooja_FL_Centralised.py
+++ Pooja_FL_Centralised.py
@@ -207,7 +207,7 @@ for client in clients:
         client['model'] = CNN().to(device)
         client['optim'] = optim.SGD(client['model'].parameters(), lr=args['lr'])
         
-print(client)
+# print(client)
 for fed_round in range(args['rounds']):
     
     # number of selected clients
@@ -225,6 +225,7 @@ for fed_round in range(args['rounds']):
     
     # Training 
     print(client)
+    print("\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\")
     for client in active_clients:
         train(args,client, device)
     
