52d0fa28482310a760a23eb0937d5886a2a14656
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2021-12-16 01:02:30 +0530

    Update Pooja_FL_Centralised.py

--- Pooja_FL_Centralised.py
+++ Pooja_FL_Centralised.py
@@ -28,7 +28,7 @@ args = {
     'batch_size' : 64,
     'test_batch_size' : 1000,
     'lr' : 0.001,
-    'log_interval' : 47,
+    'log_interval' : 48,
     'epochs' : 2,
     'clients' : 20,
     'seed' : 0,
