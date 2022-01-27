983529df1a28635a336780da9579487b4372311e
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-01-27 23:02:18 +0530

    Update Pooja_FL_Centralised.py

--- Pooja_FL_Centralised.py
+++ Pooja_FL_Centralised.py
@@ -126,6 +126,7 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds,hook):
         
         def __getitem__(self,item):
             images,labels=self.dataset[self.indx[item]]
+            print(type(torch.tensor(images)))
             return (torch.tensor(images),torch.tensor(labels))
         
         
