2eb10f9d8db53ef3cf5a50defb8da6ad863ac27e
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-02-26 04:11:47 +0530

    Update IID_Wireless_OneWay.py

--- IID_Wireless_OneWay.py
+++ IID_Wireless_OneWay.py
@@ -478,7 +478,7 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds,key,key_arr
         # print('=============\\\\\\\=====================')
         idx = 0
         power_1 = 0
-        for client in active_clients:
+        for client in clients:
             print("train")
             good_channel, power_1 = train(args,client, device,mu,csi[idx],snr[idx],key, key_array )
             if(good_channel == True):
