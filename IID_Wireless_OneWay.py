3c456917276701c46e3cd37725e72767552d72a3
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-02-26 18:31:35 +0530

    Update IID_Wireless_OneWay.py

--- IID_Wireless_OneWay.py
+++ IID_Wireless_OneWay.py
@@ -205,73 +205,75 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds,key,key_arr
             return Func.log_softmax(x, dim=1)
         
 
-    def train(args, client, device,mu,csi,snr,key,key_array):
+    def train(args, client, device):
         Client_Status = False
         client['model'].train()
-        if(csi==0 or mu==0):
-            Optimal_Power = 0
-        else:
-            Optimal_Power = max(0,(1/mu- 1/csi))
+        # if(csi==0 or mu==0):
+            # Optimal_Power = 0
+        # else:
+            # Optimal_Power = max(0,(1/mu- 1/csi))
         # Optimal_Power = max(0,(1/mu - 1/csi))   
-        print("Optimal power allocated is: ", Optimal_Power)
+        # print("Optimal power allocated is: ", Optimal_Power)
 
-        snr_val = 10**(snr/10)
+        # snr_val = 10**(snr/10)
 
-        absh = csi*Optimal_Power/snr_val
-        x=random.uniform(0,absh)
-        #std = math.sqrt(Ps/snr_val)
-        #y = random.random()
-        y=math.sqrt(absh*absh-x*x)
-        std=math.sqrt(Optimal_Power/snr_val*absh*absh)
+        # absh = csi*Optimal_Power/snr_val
+        # x=random.uniform(0,absh)
+        # #std = math.sqrt(Ps/snr_val)
+        # #y = random.random()
+        # y=math.sqrt(absh*absh-x*x)
+        # std=math.sqrt(Optimal_Power/snr_val*absh*absh)
 
-        h = complex(x,y)
-        print("snr in dB",snr )
+        # h = complex(x,y)
+        # print("snr in dB",snr )
 
-        if(Optimal_Power!= 0):
+        # if(Optimal_Power!= 0):
 
-            data= client['model'].conv1.weight
-            data = data*math.sqrt(Optimal_Power)
-            noise = torch.randn(data.size())
-            y_out = h*data + noise*std
-            y_out = y_out/(math.sqrt(Optimal_Power)*(h))
-            y_out = y_out.real
+        #     data= client['model'].conv1.weight
+        #     data = data*math.sqrt(Optimal_Power)
+        #     noise = torch.randn(data.size())
+        #     y_out = h*data + noise*std
+        #     y_out = y_out/(math.sqrt(Optimal_Power)*(h))
+        #     y_out = y_out.real
 
-            client['model'].conv1.weight.data = y_out 
+        #     client['model'].conv1.weight.data = y_out 
 
-            data= client['model'].conv2.weight
-            data = data*math.sqrt(Optimal_Power)
-            noise = torch.randn(data.size())
-            y_out = h*data + noise*std
-            y_out = y_out/(math.sqrt(Optimal_Power)*(h))
-            y_out = y_out.real
+        #     data= client['model'].conv2.weight
+        #     data = data*math.sqrt(Optimal_Power)
+        #     noise = torch.randn(data.size())
+        #     y_out = h*data + noise*std
+        #     y_out = y_out/(math.sqrt(Optimal_Power)*(h))
+        #     y_out = y_out.real
 
-            client['model'].conv2.weight.data = y_out
+        #     client['model'].conv2.weight.data = y_out
 
-        client['model'].send(client['hook'])
-        print("Client:",client['hook'].id)
+        # client['model'].send(client['hook'])
+        # print("Client:",client['hook'].id)
 
-        print("CSI",csi)
-        print()
+        # print("CSI",csi)
+        # print()
 
-        key_received = h*key_array+(np.random.randn(len(key_array))*std*2)
-        #print(key_array_received)
-        key_received=(key_received/(h)).real
+        # key_received = h*key_array+(np.random.randn(len(key_array))*std*2)
+        # #print(key_array_received)
+        # key_received=(key_received/(h)).real
         
-        for n in range (len(key_received)): 
-            if(key_received[n]>=0):
-                key_received[n]=0
-            else:
-                key_received[n]=1
+        # for n in range (len(key_received)): 
+        #     if(key_received[n]>=0):
+        #         key_received[n]=0
+        #     else:
+        #         key_received[n]=1
         
-        key_received=key_received.tolist()
-        key_received = [int(item) for item in key_received]
+        # key_received=key_received.tolist()
+        # key_received = [int(item) for item in key_received]
         
-        # print(client)
-        # iterate over federated data
+        # # print(client)
+        # # iterate over federated data
 
-        Xor_sum = sum(np.bitwise_xor(key,key_received))
-        error = Xor_sum/len(key)
-        if(error == 0 and Optimal_Power >0):
+        # Xor_sum = sum(np.bitwise_xor(key,key_received))
+        # error = Xor_sum/len(key)
+        error= 0
+        #if(error == 0 and Optimal_Power >0):
+        if(error==0):
             Client_Status = True
 
             for epoch in range(1,args['epochs']+1):
@@ -308,62 +310,62 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds,key,key_arr
             print("Channel is not taken for fedavg in this round")
         client['model'].get()
         
-        return Client_Status, Optimal_Power
+        #return Client_Status, Optimal_Power
 
-    def ClientUpdateVal(clients,key,key_array,power):
-        good_channel =[]
-        for client in clients:
-            snr=random.randint(args['lowest_snr'],args['highest_snr'])  
-            print("snr in dB = ",snr)
-            print("Client:",client['hook'].id)
-            snr_value=10**(snr/10)
-            std=math.sqrt(Ps/snr_value) #channel noise
-            x=random.random()
-            y=random.random()
-            h=complex(x,y)
+    # def ClientUpdateVal(clients,key,key_array,power):
+    #     good_channel =[]
+    #     for client in clients:
+    #         snr=random.randint(args['lowest_snr'],args['highest_snr'])  
+    #         print("snr in dB = ",snr)
+    #         print("Client:",client['hook'].id)
+    #         snr_value=10**(snr/10)
+    #         std=math.sqrt(Ps/snr_value) #channel noise
+    #         x=random.random()
+    #         y=random.random()
+    #         h=complex(x,y)
             
             
-            data=client['model'].conv1.weight
-            data=data*math.sqrt(Ps) 
+    #         data=client['model'].conv1.weight
+    #         data=data*math.sqrt(Ps) 
 
-            power += torch.norm(abs(data*data)).item()
-            noise = (torch.randn(data.size())*std)
-            y_out = h*data + noise
-            y_out = y_out/(math.sqrt(Ps)*(h)) 
-            y_out= y_out.real 
-            client['model'].conv1.weight.data=y_out
+    #         power += torch.norm(abs(data*data)).item()
+    #         noise = (torch.randn(data.size())*std)
+    #         y_out = h*data + noise
+    #         y_out = y_out/(math.sqrt(Ps)*(h)) 
+    #         y_out= y_out.real 
+    #         client['model'].conv1.weight.data=y_out
             
             
-            data=client['model'].conv2.weight
-            data=data*math.sqrt(Ps) 
-            noise1 = (torch.randn(data.size())*std)
-            y_out = h*data + noise1
-            y_out = y_out/(math.sqrt(Ps)*(h)) 
-            y_out= y_out.real 
-            client['model'].conv2.weight.data=y_out
+    #         data=client['model'].conv2.weight
+    #         data=data*math.sqrt(Ps) 
+    #         noise1 = (torch.randn(data.size())*std)
+    #         y_out = h*data + noise1
+    #         y_out = y_out/(math.sqrt(Ps)*(h)) 
+    #         y_out= y_out.real 
+    #         client['model'].conv2.weight.data=y_out
             
-            key_received=h*key_array+(np.random.randn(len(key_array))*std*2)
-            key_received=(key_received/(h)).real
+    #         key_received=h*key_array+(np.random.randn(len(key_array))*std*2)
+    #         key_received=(key_received/(h)).real
 
-            for n in range (len(key_received)): 
-                if(key_received[n]>=0):
-                    key_received[n]=0
-                else:
-                    key_received[n]=1
+    #         for n in range (len(key_received)): 
+    #             if(key_received[n]>=0):
+    #                 key_received[n]=0
+    #             else:
+    #                 key_received[n]=1
         
-            key_received=key_received.tolist()
-            key_received = [int(item) for item in key_received]
+    #         key_received=key_received.tolist()
+    #         key_received = [int(item) for item in key_received]
             
-            Xor_sum = sum(np.bitwise_xor(key_received,key))
-            error = Xor_sum/len(key)
-            if(error == 0):
-                print("This is a Good Channel")
-                good_channel.append(client)
-            else:
-                print("This is a Poor Channel")    
-            print()
+    #         Xor_sum = sum(np.bitwise_xor(key_received,key))
+    #         error = Xor_sum/len(key)
+    #         if(error == 0):
+    #             print("This is a Good Channel")
+    #             good_channel.append(client)
+    #         else:
+    #             print("This is a Poor Channel")    
+    #         print()
 
-        return good_channel, power
+    #     return good_channel, power
 
     accu = []
     def test(args,model, device, test_loader, count):
@@ -437,42 +439,42 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds,key,key_arr
         # Training 
         # print(client)
        
-        snr = [] 
-        csi = []
+        # snr = [] 
+        # csi = []
         
-        for i in range(args['clients']-1):
-            # print("csi")
-            csi.append(random.uniform(args['lowest_csi'], args['highest_csi']))
-            snr.append(random.randint(args['lowest_snr'], args['highest_snr']))
+        # for i in range(args['clients']-1):
+        #     # print("csi")
+        #     csi.append(random.uniform(args['lowest_csi'], args['highest_csi']))
+        #     snr.append(random.randint(args['lowest_snr'], args['highest_snr']))
 
-           # snr.append(random.randint())
-        print("CSI=========>>>>>>>>>>>>>",csi)
-        #===============Water Filling Algorithm ==============
-        mu_min = 1e-15
-        mu = 0
+        #    # snr.append(random.randint())
+        # print("CSI=========>>>>>>>>>>>>>",csi)
+        # #===============Water Filling Algorithm ==============
+        # mu_min = 1e-15
+        # mu = 0
 
-        wfa = 3.402823466E+38
+        # wfa = 3.402823466E+38
 
-        while(mu_min<=1):
-            wfa1 = 0
-            P_total = 0
+        # while(mu_min<=1):
+        #     wfa1 = 0
+        #     P_total = 0
 
-            for csi_i in csi:
-                # print(csi_i)
-                if(csi_i==0 or mu_min==0):
-                    P_optimal = 0
-                else:
-                    P_optimal = max(0,(1/mu_min - 1/csi_i))
-                wfa1 = math.log( 1+ P_optimal**csi_i)
-                P_total += P_optimal
+        #     for csi_i in csi:
+        #         # print(csi_i)
+        #         if(csi_i==0 or mu_min==0):
+        #             P_optimal = 0
+        #         else:
+        #             P_optimal = max(0,(1/mu_min - 1/csi_i))
+        #         wfa1 = math.log( 1+ P_optimal**csi_i)
+        #         P_total += P_optimal
             
-            len_ac = len(active_clients)
-            g = wfa1 - mu_min* (P_total - Ps*(len_ac-1))
-            if(g<wfa):
-                mu = mu_min
-                wfa = g
-            print("mu ki value", mu)
-            mu_min+= 0.00004
+        #     len_ac = len(active_clients)
+        #     g = wfa1 - mu_min* (P_total - Ps*(len_ac-1))
+        #     if(g<wfa):
+        #         mu = mu_min
+        #         wfa = g
+        #     print("mu ki value", mu)
+        #     mu_min+= 0.00004
 
 
             
@@ -481,50 +483,51 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds,key,key_arr
         power_1 = 0
         for client in active_clients:
             print("train")
-            good_channel, power_1 = train(args,client, device,mu,csi[idx],snr[idx],key, key_array )
-            if(good_channel == True):
-                client_good_channel.append(client)
-            idx = idx+1
+            #good_channel, power_1 = 
+            train(args,client, device )
+            # if(good_channel == True):
+            #     client_good_channel.append(client)
+            # idx = idx+1
 
             # print(client)
         
-        power = [] 
+    #     power = [] 
         
 
-        for csi_i in csi :
-            power.append(max(0,(1/mu - 1/csi_i)))
-        # fig,ax=plt.subplots()
-        # line1=ax.plot(csi,power,label="channel power allocated")
-        # line2=ax.plot(csi,[1/mu_min]*len(csi),label="maximum power allocated")
-        # ax.set_title("csi vs power allocated")
-        # ax.set_xlabel("csi (channel gain to noise ratio)")
-        # ax.set_ylabel("power allocated")
-        # ax.legend()
-        # plt.show()    
-    #     # Testing 
-    #     for client in active_clients:
-    #         test(args, client['model'], device, client['testset'], client['hook'].id)
+    #     for csi_i in csi :
+    #         power.append(max(0,(1/mu - 1/csi_i)))
+    #     # fig,ax=plt.subplots()
+    #     # line1=ax.plot(csi,power,label="channel power allocated")
+    #     # line2=ax.plot(csi,[1/mu_min]*len(csi),label="maximum power allocated")
+    #     # ax.set_title("csi vs power allocated")
+    #     # ax.set_xlabel("csi (channel gain to noise ratio)")
+    #     # ax.set_ylabel("power allocated")
+    #     # ax.legend()
+    #     # plt.show()    
+    # #     # Testing 
+    # #     for client in active_clients:
+    # #         test(args, client['model'], device, client['testset'], client['hook'].id)
 
-        print()
-        csi.sort()
+    #     print()
+    #     csi.sort()
 
-        print("Clients with good channel are considered")
-        for no in range (len(client_good_channel)):
-            print(client_good_channel[no]['hook'].id)
+    #     print("Clients with good channel are considered")
+    #     for no in range (len(client_good_channel)):
+    #         print(client_good_channel[no]['hook'].id)
 
 
-        print()
-        print("Sending data back to Server")
-        print() 
+    #     print()
+    #     print("Sending data back to Server")
+    #     print() 
 
-        #ClientUpdateVal(clients,key,key_array,power_client)
-        #good_channel_odd,power_odd=ClientUpdateVal(client_good_channel,key,key_array,0)
+    #     #ClientUpdateVal(clients,key,key_array,power_client)
+    #     #good_channel_odd,power_odd=ClientUpdateVal(client_good_channel,key,key_array,0)
     
-        print()
+    #     print()
 
-        print("Clients having a good channel and considered for averaging")
-        # for no in range (len(good_channel_odd)):
-        #     print(good_channel_odd[no]['hook'].id)
+    #     print("Clients having a good channel and considered for averaging")
+    #     # for no in range (len(good_channel_odd)):
+    #     #     print(good_channel_odd[no]['hook'].id)
         # Averaging 
         global_model = averageModels(global_model,client_good_channel)
         
