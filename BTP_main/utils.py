af5789d886c53ce213dfbf56061e26e4106b5028
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-02-26 22:17:29 +0530

    Update utils.py

--- BTP_main/utils.py
+++ BTP_main/utils.py
@@ -1,11 +1,15 @@
+import torch
+
+
 def averageModels(global_model, clients):
     client_models = [clients[i]['model'] for i in range(len(clients))]
     samples = [clients[i]['samples'] for i in range(len(clients))]
     global_dict = global_model.state_dict()
-        
-    for k in global_dict.keys(): #key is CNN layer index and value is layer parameters
-        global_dict[k] = torch.stack([client_models[i].state_dict()[k].float() * samples[i] for i in range(len(client_models))], 0).sum(0) #take a weighted average and not average because the clients may not have the same amount of data to train upon
-                
+
+    for k in global_dict.keys():  # key is CNN layer index and value is layer parameters
+        # take a weighted average and not average because the clients may not have the same amount of data to train upon
+        global_dict[k] = torch.stack([client_models[i].state_dict()[k].float(
+        ) * samples[i] for i in range(len(client_models))], 0).sum(0)
+
     global_model.load_state_dict(global_dict)
     return global_model
-
