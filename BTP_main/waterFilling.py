d96d1bfc46f64a4e093d669f0f25b3e76fdfd946
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-03-01 01:19:51 +0530

    Update waterFilling.py

--- BTP_main/waterFilling.py
+++ BTP_main/waterFilling.py
@@ -443,7 +443,7 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds, key, key_a
 
         # print('=============\\\\\\\=====================')
         idx = 0
-        power_1 = 0
+        power_1 = []
         for client in clients:
             print("train")
             good_channel = train(args, client, device,
