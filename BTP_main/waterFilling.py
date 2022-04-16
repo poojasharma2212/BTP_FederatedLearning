109a9a9301be81d18fccf4f1bac656c9b5aac726
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-04-17 00:23:10 +0530

    Update waterFilling.py

--- BTP_main/waterFilling.py
+++ BTP_main/waterFilling.py
@@ -49,7 +49,7 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds, key, key_a
         'epochs': 2,
         'clients': 20,
         'seed': 0,
-        'rounds': 15,
+        'rounds': 60,
         'C': 0.9,
         'lowest_snr': 0,
         'highest_snr': 38,
