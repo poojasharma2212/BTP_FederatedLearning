728f58e0e8bc3a58bb3379ee28cc8fa7dcba9f41
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-05-11 13:38:26 +0530

    Update IID_Wireless_OneWay.py

--- BTP_main/IID_Wireless_OneWay.py
+++ BTP_main/IID_Wireless_OneWay.py
@@ -205,7 +205,7 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds, key, key_a
         # if(error == 0 and Optimal_Power >0):
         print("The error we get ")
         print(error)
-        if(round(error) == 0):
+        if((error) == 0):
             cStatus = True     # Client status
             for epoch in range(1, args['epochs']+1):
                 for batch_idx, (data, target) in enumerate(client['mnist_trainset']):
