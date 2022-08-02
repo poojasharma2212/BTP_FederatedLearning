c72f190b29a1cfb23ea5ee38afce0c3667c767e1
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-08-03 02:10:46 +0530

    Update IID_Wireless_OneWay.py

--- BTP_main/IID_Wireless_OneWay.py
+++ BTP_main/IID_Wireless_OneWay.py
@@ -173,7 +173,7 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds, key, key_a
         x = torch.flatten(data)
         xTx = 0
         print("************8")
-        print(data.size())
+        # print(data.size())
         # print(x.size())
         for i in range(x.shape()):
             xTx = xTx + x[i]*x[i]
