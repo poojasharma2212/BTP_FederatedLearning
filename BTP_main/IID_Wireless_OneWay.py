9ff9ec3cfaa42cfc4ad896d2a39ad37d0066c364
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-08-03 01:27:42 +0530

    Update IID_Wireless_OneWay.py

--- BTP_main/IID_Wireless_OneWay.py
+++ BTP_main/IID_Wireless_OneWay.py
@@ -200,7 +200,7 @@ def Wrapper(batch_size, lr, no_of_epoch, no_of_clients, no_of_rounds, key, key_a
         # print("000000000000000000000000")
         # print(xx*transposed)
 
-        data = data*math.sqrt(Ps)/(h)*(xTx)
+        data = data*math.sqrt(Ps)/((h)*(xTx))
         # print(data)
         noise = torch.randn(data.size())
         y_out = h*data + noise*std
