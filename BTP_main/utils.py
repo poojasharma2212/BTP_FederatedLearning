341935bcf9f50352de5b6ad5676278a902801a1a
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-08-09 02:02:11 +0530

    Update utils.py

--- BTP_main/utils.py
+++ BTP_main/utils.py
@@ -20,6 +20,6 @@ def averageModels(global_model, clients, snr_value, Ps):
         global_dict[k] = torch.stack([client_models[i].state_dict()[k].float(
         ) * samples[i] for i in range(len(client_models))], 0).sum(0)
 
-    print(global_model)
+    print(global_model.load_state_dict(global_dict))
     global_model.load_state_dict(global_dict)
     return global_model
