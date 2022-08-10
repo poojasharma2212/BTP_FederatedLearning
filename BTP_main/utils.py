85eea2427b30633035e8dca547a0e7bf561951b1
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-08-10 05:38:15 +0530

    Update utils.py

--- BTP_main/utils.py
+++ BTP_main/utils.py
@@ -11,12 +11,12 @@ def averageModels(global_model, clients, snr_value, Ps):
     for k in global_dict.keys():  # key is CNN layer index and value is layer parameters
         # take a weighted average and not average because the clients may not have the same amount of data to train upon
         snr = snr_value
-        print("SNR==", snr)
+        # print("SNR==", snr)
         snr_val = 10**(snr/10)
         std = math.sqrt(Ps/snr_val)
         # print("Client", client_models)
         print("-----------------")
-        print(std)
+        # print(std)
         # print(global_dict)
         # noise = torch.randn(global_dict.size())
         global_dict[k] = torch.stack([client_models[i].state_dict()[k].float(
