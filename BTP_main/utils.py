9d6a392856a907860fc0cc274cde00dd110f7c08
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-08-21 22:41:55 +0530

    Update utils.py

--- BTP_main/utils.py
+++ BTP_main/utils.py
@@ -14,11 +14,6 @@ def averageModels(global_model, clients, snr_value, Ps):
         # print("SNR==", snr)
         snr_val = 10**(snr/10)
         std = math.sqrt(Ps/snr_val)
-        # print("Client", client_models)
-        print("-----------------")
-        # print(std)
-        # print(global_dict)
-        # noise = torch.randn(global_dict.size())
         global_dict[k] = torch.stack([client_models[i].state_dict()[k].float(
         ) * samples[i] for i in range(len(client_models))], 0).sum(0)
 
@@ -35,35 +30,18 @@ def averageModels(global_model, clients, snr_value, Ps):
     print('-----------')
     print("newModel Normalised value : ", xTx)
     print(xTx)
-    # if(xTx <= Ps):
-    # Ps = Ps/xTx
-    y_out = y_out*math.sqrt(Ps)
-    # else:
-    # y_out = y_out*math.sqrt(Ps)/((h)*xTx)
+    # y_out = y_out*math.sqrt(Ps)
     noise = torch.randn(y_out.size())
     y_out = y_out + noise*std
-    y_out = y_out/(math.sqrt(Ps))
-    # y_out = y_out.real
+    # y_out = y_out/(math.sqrt(Ps))
 
     global_model.conv1.weight.data = y_out
 
     y_out = global_model.conv2.weight
-    yy = torch.flatten(y_out)
-    yTy = 0
-    for i in range(list(yy.size())[0]):
-        yTy = yTy + yy[i]*yy[i]
-
-    print('-----------')
-    print("xTTTTTTTTTTTTx: ", yTy)
-    print(yTy)
-    # Ps = Ps/yTy
-    # if(yTy <= Ps):
-    y_out = y_out*math.sqrt(Ps)
-    # else:
-    # y_out = y_out*math.sqrt(Ps)/((h)*yTy)
+    # y_out = y_out*math.sqrt(Ps)
     noise = torch.randn(y_out.size())
     y_out = y_out + noise*std
-    y_out = y_out/(math.sqrt(Ps))
+    # y_out = y_out/(math.sqrt(Ps))
 
     global_model.conv2.weight.data = y_out
 
