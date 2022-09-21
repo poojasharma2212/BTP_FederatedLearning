cc3884c29e0f7bf74ddb3101d57c3838902c738d
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-09-22 04:57:18 +0530

    Update IID_Wireless_OneWay.py

--- BTP_main/IID_Wireless_OneWay.py
+++ BTP_main/IID_Wireless_OneWay.py
@@ -55,7 +55,7 @@ def Wrapper():
         'seed': 0,
         'rounds': 150,
         'C': 0.9,
-        'lowest_snr': 3,
+        'lowest_snr': 4,
         # 'highest_snr': 20,
         'lowest_csi': 0,
         'highest_csi': 1,
