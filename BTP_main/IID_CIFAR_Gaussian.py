04defc93fb11fd5f0795ac437cb490f22dfdeece
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-10-10 23:35:51 +0530

    Update IID_CIFAR_Gaussian.py

--- BTP_main/IID_CIFAR_Gaussian.py
+++ BTP_main/IID_CIFAR_Gaussian.py
@@ -82,18 +82,22 @@ def Wrapper():
     # print(clients)
     # os.chdir("/content/drive/MyDrive/FL_ZaaPoo/data/MNIST/raw")
     nUsers = 30
-    transform = transforms.Compose([
+    transform_train = transforms.Compose([
     transforms.RandomCrop(32, padding=4),
     transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
+
+    transform_test = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),])
     
     cifar_trainset = datasets.CIFAR10(root='./data', train=True, download=True, transform=transform_train)
-    trainset_loader = torch.utils.data.DataLoader(cifar_trainset, batch_size=batch_size,shuffle=True, num_workers=num_workers)
+    # trainset_loader = torch.utils.data.DataLoader(cifar_trainset, batch_size=batch_size,shuffle=True, num_workers=num_workers)
 
     cifar_testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
-    testset_loader = torch.utils.data.DataLoader(cifar_testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    # testset_loader = torch.utils.data.DataLoader(cifar_testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
     # transform = transforms.Compose(
     #     [transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
@@ -132,7 +136,7 @@ def Wrapper():
         [transforms.ToTensor(), transforms.Normalize((0.4914,0.4822,0.4465), (0.2023, 0.1994, 0.2010))])
     
     global_test_dataset = datasets.CIFAR10(
-        './', train=False, download=True, transform=transform)
+        './', train=False, download=True, transform=transform_train)
     
     global_test_loader = DataLoader(
         global_test_dataset, batch_size=args['batch_size'], shuffle=True)
