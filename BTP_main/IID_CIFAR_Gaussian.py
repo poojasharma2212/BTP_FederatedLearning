62a03dab094263783b873d92b7b879e59609f63c
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-10-10 23:40:00 +0530

    Update IID_CIFAR_Gaussian.py

--- BTP_main/IID_CIFAR_Gaussian.py
+++ BTP_main/IID_CIFAR_Gaussian.py
@@ -83,8 +83,8 @@ def Wrapper():
     # os.chdir("/content/drive/MyDrive/FL_ZaaPoo/data/MNIST/raw")
     nUsers = 30
     transform_train = transforms.Compose([
-    transforms.RandomCrop(32, padding=4),
-    transforms.RandomHorizontalFlip(),
+    # transforms.RandomCrop(32, padding=4),
+    # transforms.RandomHorizontalFlip(),
     transforms.ToTensor(),
     transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
     ])
