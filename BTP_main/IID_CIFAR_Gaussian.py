3e037c856eb22880dfd51f65d465fdb9b867c2f1
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-10-11 00:16:26 +0530

    Update IID_CIFAR_Gaussian.py

--- BTP_main/IID_CIFAR_Gaussian.py
+++ BTP_main/IID_CIFAR_Gaussian.py
@@ -146,23 +146,48 @@ def Wrapper():
     class CNN(nn.Module):
         def __init__(self):
             super(CNN, self).__init__()
-            self.conv1 = nn.Conv2d(1, 20, 5, 1)
-            self.conv2 = nn.Conv2d(20, 50, 5, 1)
-            self.fc1 = nn.Linear(4*4*50, 500)
-            self.fc2 = nn.Linear(500, 10)
-
-            # self.noise_conv1 = torch.randn(nn.Parameter(self.conv1.weight).size())*std + 0
-            # self.noise_conv2 = torch.randn(nn.Parameter(self.conv2.weight).size())*std + 0
+            self.conv1 = nn.Conv2d(in_channels=3, out_channels=48, kernel_size=(3,3), padding=(1,1))
+            self.conv2 = nn.Conv2d(in_channels=48, out_channels=96, kernel_size=(3,3), padding=(1,1))
+            self.conv3 = nn.Conv2d(in_channels=96, out_channels=192, kernel_size=(3,3), padding=(1,1))
+            self.conv4 = nn.Conv2d(in_channels=192, out_channels=256, kernel_size=(3,3), padding=(1,1))
+            self.pool = nn.MaxPool2d(2,2)
+            self.fc1 = nn.Linear(in_features=8*8*256, out_features=512)
+            self.fc2 = nn.Linear(in_features=512, out_features=64)
+            self.Dropout = nn.Dropout(0.25)
+            self.fc3 = nn.Linear(in_features=64, out_features=10)
 
         def forward(self, x):
-            x = Func.relu(self.conv1(x))
-            x = Func.max_pool2d(x, 2, 2)
-            x = Func.relu(self.conv2(x))
-            x = Func.max_pool2d(x, 2, 2)
-            x = x.view(-1, 4*4*50)
+            x = Func.relu(self.conv1(x)) #32*32*48
+            x = Func.relu(self.conv2(x)) #32*32*96
+            x = self.pool(x) #16*16*96
+            x = self.Dropout(x)
+            x = Func.relu(self.conv3(x)) #16*16*192
+            x = Func.relu(self.conv4(x)) #16*16*256
+            x = self.pool(x) # 8*8*256
+            x = self.Dropout(x)
+            x = x.view(-1, 8*8*256) # reshape x
             x = Func.relu(self.fc1(x))
-            x = self.fc2(x)
-            return Func.log_softmax(x, dim=1)
+            x = Func.relu(self.fc2(x))
+            x = self.Dropout(x)
+            x = self.fc3(x)
+            return x
+        #     self.conv1 = nn.Conv2d(1, 20, 5, 1)
+        #     self.conv2 = nn.Conv2d(20, 50, 5, 1)
+        #     self.fc1 = nn.Linear(4*4*50, 500)
+        #     self.fc2 = nn.Linear(500, 10)
+
+        #     # self.noise_conv1 = torch.randn(nn.Parameter(self.conv1.weight).size())*std + 0
+        #     # self.noise_conv2 = torch.randn(nn.Parameter(self.conv2.weight).size())*std + 0
+
+        # def forward(self, x):
+        #     x = Func.relu(self.conv1(x))
+        #     x = Func.max_pool2d(x, 2, 2)
+        #     x = Func.relu(self.conv2(x))
+        #     x = Func.max_pool2d(x, 2, 2)
+        #     x = x.view(-1, 4*4*50)
+        #     x = Func.relu(self.fc1(x))
+        #     x = self.fc2(x)
+        #     return Func.log_softmax(x, dim=1)
 
     x_dict = {}
     y_dict = {}
