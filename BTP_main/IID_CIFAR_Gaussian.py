9aff3916d54b841475c6d5e4c1069a41d748f109
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-10-13 00:38:50 +0530

    Update IID_CIFAR_Gaussian.py

--- BTP_main/IID_CIFAR_Gaussian.py
+++ BTP_main/IID_CIFAR_Gaussian.py
@@ -146,10 +146,10 @@ def Wrapper():
     class CNN(nn.Module):
         def __init__(self):
             super(CNN, self).__init__()
-            self.conv1 = nn.Conv2d(in_channels=3, out_channels=48, kernel_size=(3,3), padding=(1,1))
-            self.conv2 = nn.Conv2d(in_channels=48, out_channels=96, kernel_size=(3,3), padding=(1,1))
-            self.conv3 = nn.Conv2d(in_channels=96, out_channels=192, kernel_size=(3,3), padding=(1,1))
-            self.conv4 = nn.Conv2d(in_channels=192, out_channels=256, kernel_size=(3,3), padding=(1,1))
+            self.conv1 = nn.Conv2d(3, 64, kernel_size=(3,3), padding=(1,1))
+            self.conv2 = nn.Conv2d(64,96, kernel_size=(3,3), padding=(1,1))
+            self.conv3 = nn.Conv2d(96,192, kernel_size=(3,3), padding=(1,1))
+            self.conv4 = nn.Conv2d(192,256, kernel_size=(3,3), padding=(1,1))
             self.pool = nn.MaxPool2d(2,2)
             self.fc1 = nn.Linear(in_features=8*8*256, out_features=512)
             self.fc2 = nn.Linear(in_features=512, out_features=64)
