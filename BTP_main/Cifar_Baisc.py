d3398dc0d72d8e83c199bfd52a49ee92a5e066f8
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-10-13 04:30:11 +0530

    Update Cifar_Baisc.py

--- BTP_main/Cifar_Baisc.py
+++ BTP_main/Cifar_Baisc.py
@@ -49,7 +49,7 @@ class ConvNet(nn.Module):
         self.conv3 = nn.Conv2d(64, 128, 5, padding=(1,1))
         # self.conv4 = nn.Conv2d(64, 256, 5, padding=(1,1))
         self.pool = nn.MaxPool2d(2,2)
-        self.conv5 = nn.Conv2d(128,256, 10, padding=(1,1))
+        self.conv4 = nn.Conv2d(128,256, 10, padding=(1,1))
         self.fc1 = nn.Linear(in_features=5*5*256, out_features=512)
         self.fc2 = nn.Linear(in_features=512, out_features=64)
         self.Dropout = nn.Dropout(0.15)
@@ -62,8 +62,9 @@ class ConvNet(nn.Module):
         x = self.Dropout(x)
         x = F.relu(self.conv3(x)) #16*16*192
         # x = F.relu(self.conv4(x)) #16*16*256
-        x = F.relu(self.conv5(x)) #16*16*256
+        
         x = self.pool(x) # 8*8*256
+        x = F.relu(self.conv4(x)) #16*16*256
         x = self.Dropout(x)
        
         x = x.view(-1, 5*5*256) # reshape x
