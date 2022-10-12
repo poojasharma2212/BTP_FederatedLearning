f94b57609dee4cf2e0f9105b1013c7aa80e51614
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-10-13 04:30:44 +0530

    Update Cifar_Baisc.py

--- BTP_main/Cifar_Baisc.py
+++ BTP_main/Cifar_Baisc.py
@@ -62,9 +62,9 @@ class ConvNet(nn.Module):
         x = self.Dropout(x)
         x = F.relu(self.conv3(x)) #16*16*192
         # x = F.relu(self.conv4(x)) #16*16*256
-        
-        x = self.pool(x) # 8*8*256
         x = F.relu(self.conv4(x)) #16*16*256
+        x = self.pool(x) # 8*8*256
+        
         x = self.Dropout(x)
        
         x = x.view(-1, 5*5*256) # reshape x
