9557b54ef7fbc2d1c5a1d1aa306044f166ada340
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-10-12 16:54:59 +0530

    Update Cifar_Baisc.py

--- BTP_main/Cifar_Baisc.py
+++ BTP_main/Cifar_Baisc.py
@@ -41,18 +41,47 @@ trainset_loader = torch.utils.data.DataLoader(cifar_trainset, batch_size=batch_s
 cifar_testset = datasets.CIFAR10(root='./data', train=False, download=True, transform=transform_test)
 testset_loader = torch.utils.data.DataLoader(cifar_testset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
 
+# class ConvNet(nn.Module):
+#     def __init__(self):
+#         super(ConvNet, self).__init__()
+#         self.conv1 = nn.Conv2d(in_channels=3, out_channels=48, kernel_size=(3,3), padding=(1,1))
+#         self.conv2 = nn.Conv2d(in_channels=48, out_channels=96, kernel_size=(3,3), padding=(1,1))
+#         self.conv3 = nn.Conv2d(in_channels=96, out_channels=192, kernel_size=(3,3), padding=(1,1))
+#         self.conv4 = nn.Conv2d(in_channels=192, out_channels=256, kernel_size=(3,3), padding=(1,1))
+#         self.pool = nn.MaxPool2d(2,2)
+#         self.fc1 = nn.Linear(in_features=8*8*256, out_features=512)
+#         self.fc2 = nn.Linear(in_features=512, out_features=64)
+#         self.Dropout = nn.Dropout(0.25)
+#         self.fc3 = nn.Linear(in_features=64, out_features=10)
+
+#     def forward(self, x):
+#         x = F.relu(self.conv1(x)) #32*32*48
+#         x = F.relu(self.conv2(x)) #32*32*96
+#         x = self.pool(x) #16*16*96
+#         x = self.Dropout(x)
+#         x = F.relu(self.conv3(x)) #16*16*192
+#         x = F.relu(self.conv4(x)) #16*16*256
+#         x = self.pool(x) # 8*8*256
+#         x = self.Dropout(x)
+#         x = x.view(-1, 8*8*256) # reshape x
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         x = self.Dropout(x)
+#         x = self.fc3(x)
+#         return x
+
 class ConvNet(nn.Module):
     def __init__(self):
         super(ConvNet, self).__init__()
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=48, kernel_size=(3,3), padding=(1,1))
-        self.conv2 = nn.Conv2d(in_channels=48, out_channels=96, kernel_size=(3,3), padding=(1,1))
-        self.conv3 = nn.Conv2d(in_channels=96, out_channels=192, kernel_size=(3,3), padding=(1,1))
-        self.conv4 = nn.Conv2d(in_channels=192, out_channels=256, kernel_size=(3,3), padding=(1,1))
+        self.conv1 = nn.Conv2d(3,48,(3,3),(1,1))
+        self.conv2 = nn.Conv2d(48,96, (3,3), (1,1))
+        self.conv3 = nn.Conv2d(96,192, (3,3), (1,1))
+        self.conv4 = nn.Conv2d(192,256, (3,3),(1,1))
         self.pool = nn.MaxPool2d(2,2)
-        self.fc1 = nn.Linear(in_features=8*8*256, out_features=512)
-        self.fc2 = nn.Linear(in_features=512, out_features=64)
+        self.fc1 = nn.Linear(8*8*256, 512)
+        self.fc2 = nn.Linear(512, 64)
         self.Dropout = nn.Dropout(0.25)
-        self.fc3 = nn.Linear(in_features=64, out_features=10)
+        self.fc3 = nn.Linear(64,10)
 
     def forward(self, x):
         x = F.relu(self.conv1(x)) #32*32*48
