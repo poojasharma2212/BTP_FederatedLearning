6d1f3c2b1390440677e58617eb32a346d6c81378
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2022-10-28 05:09:34 +0530

    Update Non_IID_ImpulsiveNoise.py

--- BTP_main/Non_IID_ImpulsiveNoise.py
+++ BTP_main/Non_IID_ImpulsiveNoise.py
@@ -45,7 +45,7 @@ def Wrapper():
     # print("yes")
 
     args = {
-        'batch_size': 64,
+        'batch_size': 32,
         'test_batch_size': 1000,
         'lr': 0.05 ,
         'log_interval': 10,
