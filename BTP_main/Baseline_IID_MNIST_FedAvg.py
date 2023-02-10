33d3450222afdd1e50fee962c6bd9cb3096d8624
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2023-02-10 05:44:06 +0530

    Update Baseline_IID_MNIST_FedAvg.py

--- BTP_main/Baseline_IID_MNIST_FedAvg.py
+++ BTP_main/Baseline_IID_MNIST_FedAvg.py
@@ -354,12 +354,12 @@ def Wrapper():
             # x1 = np.random.normal(loc=0, scale=sigma1, size = 10000)
             # x2 = np.random.normal(loc=0, scale=sigma2, size = 10000)
             # x = vstack((x1, x2))
-            cov = tf.identity([500])
-            mean = tf.zeros([500])
-            x = np.random.multivariate_normal(0, 5*cov)
+            cov = tf.identity([20, 1, 5, 5])
+            mean = tf.zeros([20,1,5,5])
+            x = np.random.multivariate_normal(mean, 5*cov)
             x.shape
             print(x.size())
-            prev = x
+            prev = 0
 
         for client in active_clients:
             print("train")
