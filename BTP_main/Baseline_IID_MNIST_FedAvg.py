1c8e316792645d40517b4d9f8b025c4a2de1e0c1
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2023-02-12 12:45:25 +0530

    Update Baseline_IID_MNIST_FedAvg.py

--- BTP_main/Baseline_IID_MNIST_FedAvg.py
+++ BTP_main/Baseline_IID_MNIST_FedAvg.py
@@ -333,10 +333,10 @@ def Wrapper():
         if(fed_round == 0):
 
             # cov = nn.Identity([20,1,5,5])
-            cov = torch.eye(25000)
+            cov = torch.eye(500)
             # cov = np.eye([20,1,5,5])
             # mean = nn.zeros([20,1,5,5])
-            mean = torch.zeros(25000)
+            mean = torch.zeros(500)
             xyy = np.random.multivariate_normal(mean, 5*cov)
             # x = torch.flatten(xyy)
             # x = np.random.multivariate_normal(mean, cov).T
