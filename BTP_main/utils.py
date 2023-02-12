255d5bc369fb5e26e33c2895b52a888617ba44d0
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2023-02-12 11:32:58 +0530

    Update utils.py

--- BTP_main/utils.py
+++ BTP_main/utils.py
@@ -2,9 +2,10 @@ import torch
 import math
 
 
-def averageModels(global_model, clients, snr_value, Ps):
+def averageModels(global_model, clients, snr_value, Ps,alpha):
     client_models = [clients[i]['model'] for i in range(len(clients))]
     samples = [clients[i]['samples'] for i in range(len(clients))]
+
     global_dict = global_model.state_dict()
 
     # print('global_dict', global_dict)
@@ -22,15 +23,15 @@ def averageModels(global_model, clients, snr_value, Ps):
     global_model.load_state_dict(global_dict)
 
 
-    y_out = global_model.conv1.weight
-    x = torch.flatten(y_out)
-    xTx = 0
-    # should I use here also normalise ??
-    for i in range(list(x.size())[0]):
-        xTx = xTx + x[i]*x[i]
+    # y_out = global_model.conv1.weight
+    # x = torch.flatten(y_out)
+    # xTx = 0
+    # # should I use here also normalise ??
+    # for i in range(list(x.size())[0]):
+    #     xTx = xTx + x[i]*x[i]
 
-    print('-----------')
-    print("newModel Normalised value : ", xTx)
+    # print('-----------')
+    # print("newModel Normalised value : ", xTx)
     # print(xTx)
     # # y_out = y_out*math.sqrt(Ps)
     # noise = torch.randn(y_out.size())
