7250b81f36f38f9089ae8eb9cd527958289e6e28
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2023-03-28 07:35:50 +0530

    Update utils.py

--- BTP_main/utils.py
+++ BTP_main/utils.py
@@ -9,6 +9,10 @@ def averageModels(global_model, clients, snr_value, Ps,alpha,K_clients):
 
     global_dict = global_model.state_dict()
 
+    #  for k in global_dict.keys(): #key is CNN layer index and value is layer parameters
+    #     global_dict[k] = torch.stack([client_models[i].state_dict()[k].float() * samples[i]+torch.randn(client_models[0].state_dict()[k].float().size())*std for i in range(len(client_models))], 0).sum(0) #take a weighted average and not average because the clients may not have the same amount of data to train upon
+    #     # print(client_models[0].state_dict()[k])
+    #     #print(len(client_models))
     # print('global_dict', global_dict)
     for k in global_dict.keys():  # key is CNN layer index and value is layer parameters
         # take a weighted average and not average because the clients may not have the same amount of data to train upon
@@ -18,7 +22,8 @@ def averageModels(global_model, clients, snr_value, Ps,alpha,K_clients):
         std = math.sqrt(Ps/snr_val)
 
         global_dict[k] = torch.stack([client_models[i].state_dict()[k].float() * samples[i]  + torch.randn(client_models[0].state_dict()[k].float().size())*std for i in range(len(client_models))], 0).sum(0)
-
+        # global_dict[k] = torch.stack([client_models[i].state_dict()[k].float() * samples[i]+   torch.randn(client_models[0].state_dict()[k].float().size())*std for i in range(len(client_models))], 0).sum(0) #take a weighted average and not average because the clients may not have the same amount of data to train upon
+        print(client_models[0].state_dict())
     # print(global_dict)
     # torch.flatten(global_model)
     global_model.load_state_dict(global_dict)
