b3018681205dff585f68e32c7915e7e5a6298382
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2023-04-02 05:41:56 +0530

    Update utils.py

--- BTP_main/utils.py
+++ BTP_main/utils.py
@@ -21,7 +21,7 @@ def averageModels(global_model, clients, snr_value, Ps,alpha,K_clients):
         snr_val = 10**(snr/10)
         std = math.sqrt(Ps/snr_val)
 
-        global_dict[k] = torch.stack([client_models[i].state_dict()[k].float() * samples[i]  + torch.randn(client_models[k].state_dict()[k].float().size())*std for i in range(len(client_models))], 0).sum(0)
+        global_dict[k] = torch.stack([client_models[i].state_dict()[k].float() * samples[i]  + torch.randn(client_models[0].state_dict().float().size())*std for i in range(len(client_models))], 0).sum(0)
         # global_dict[k] = torch.stack([client_models[i].state_dict()[k].float() * samples[i]+   torch.randn(client_models[0].state_dict()[k].float().size())*std for i in range(len(client_models))], 0).sum(0) 
         # #take a weighted average and not average because the clients may not have the same amount of data to train upon
         print(client_models[0].state_dict()[k].float().size())
