c263f470e7d6de5edec33a2ef89487a37bf011d8
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2023-04-03 21:22:55 +0530

    Update Baseline_IID_MNIST_FedAvg.py

--- BTP_main/Baseline_IID_MNIST_FedAvg.py
+++ BTP_main/Baseline_IID_MNIST_FedAvg.py
@@ -8,7 +8,7 @@ import tensorflow as tf
 from torch.utils.data.dataset import Dataset
 import numpy as np
 from datetime import datetime
-# import torchvision
+import torchvision
 # print(torchvision.__version__)
 from torchvision import transforms, datasets
 from torch.utils.data import DataLoader, Dataset
