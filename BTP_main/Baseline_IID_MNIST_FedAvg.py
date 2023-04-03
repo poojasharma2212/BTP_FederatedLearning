924a2a6a83ecc95bb9975e8aa58569fe662aa8d6
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2023-04-04 05:22:08 +0530

    Update Baseline_IID_MNIST_FedAvg.py

--- BTP_main/Baseline_IID_MNIST_FedAvg.py
+++ BTP_main/Baseline_IID_MNIST_FedAvg.py
@@ -4,7 +4,7 @@ import torch.nn as nn
 import torch.optim as optim
 import torch.nn.functional as Func
 # import matplotlib.pyplot as plt
-import tensorflow as tf
+# import tensorflow as tf
 from torch.utils.data.dataset import Dataset
 import numpy as np
 from datetime import datetime
