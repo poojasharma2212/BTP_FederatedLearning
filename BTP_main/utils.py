e5074e56607922dda87726dac5a929400f6225b6
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2023-04-05 07:43:22 +0530

    Update utils.py

--- BTP_main/utils.py
+++ BTP_main/utils.py
@@ -20,8 +20,9 @@ def averageModels(global_model, clients, snr_value, Ps,alpha,K_clients):
         global_dict[k] = weighted_sum
 
         # Add Gaussian noise to the global model's parameters
-        noise = torch.randn(global_dict[k].shape) * (std/(K_clients))
-        print(noise.size())
+        # noise = torch.randn(global_dict[k].shape) * (std/(K_clients))
+        noise = 0
+        # print(noise.size())
         global_dict[k] += noise
 
     # global_model.load_state_dict(global_dict)
@@ -38,31 +39,3 @@ def averageModels(global_model, clients, snr_value, Ps,alpha,K_clients):
     global_model.load_state_dict(global_dict)
 
     return global_model
-
-
-    # y_out = global_model.conv1.weight
-    # x = torch.flatten(y_out)
-    # xTx = 0
-    # # should I use here also normalise ??
-    # for i in range(list(x.size())[0]):
-    #     xTx = xTx + x[i]*x[i]
-
-    # print('-----------')
-    # print("newModel Normalised value : ", xTx)
-    # print(xTx)
-    # # y_out = y_out*math.sqrt(Ps)
-    # noise = torch.randn(y_out.size())
-    # y_out = y_out + noise*std
-    # y_out = y_out/(math.sqrt(Ps)*27)
-
-    # global_model.conv1.weight.data = y_out
-
-    # y_out = global_model.conv2.weight
-    # # y_out = y_out*math.sqrt(Ps)
-    # noise = torch.randn(y_out.size())
-    # y_out = y_out + noise*std
-    # y_out = y_out/(math.sqrt(Ps)*27)
-
-    # global_model.conv2.weight.data = y_out
-
-    # print(global_model)
