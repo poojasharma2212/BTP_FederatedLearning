2bca6c8930b81db48425fde6e6142a7b9c3406c9
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2023-04-25 13:18:27 +0530

    Update utils.py

--- BTP_main/utils.py
+++ BTP_main/utils.py
@@ -22,9 +22,36 @@ def averageModels(global_model, clients, snr_value, Ps,alpha,K_clients,fed_round
         # Add Gaussian noise to the global model's parameters
         # noise = torch.randn(global_dict[k].shape) * (std/(K_clients))
         
+        # noise = 0
+            
+        if(fed_round == 5): #randomise round -- adding impulsive noise in random round
+            a0 = 0
+            a1 = 1
+                
+        else:
+            a0 = 1
+            a1 = 0
 
+        # print("Guassian value : ", a0)
+        std1 = math.sqrt(Ps/(snr_val)) 
+        std2 = 50*std1
+
+            # #std1 = math.sqrt(0.02/(a0+50*a1))
+            # # print(Ps/(snr_val*(a0+50*a1)))
+
+            # printx("std1",std1)
+        
+    # std1 = 0.1
+        n1 = torch.randn(torch.tensor(list(global_dict.values())).shape) * std1        
+        # n1 = torch.randn(global_dict.keys.size())*std1
+        # n2 = torch.randn(global_dict.keys.size())*std2
+        
+        n2 = torch.randn(torch.tensor(list(global_dict.values())).shape) * std2  
+        noise = a0*n1 + a1*n2
+
+        # global_dict += noise
         # print(noise.size())
-        # global_dict[k] += noise/(K_clients)
+        global_dict[k] += noise/(K_clients)
     
         # h = 1
         # y_out = global_model.conv2.weight
@@ -40,34 +67,7 @@ def averageModels(global_model, clients, snr_value, Ps,alpha,K_clients,fed_round
         # impulsive noise is added here
         # y_out = h*y_out + noise
 
-    # noise = 0
-        
-    if(fed_round == 5): #randomise round -- adding impulsive noise in random round
-        a0 = 0
-        a1 = 1
-            
-    else:
-        a0 = 1
-        a1 = 0
 
-    # print("Guassian value : ", a0)
-    std1 = math.sqrt(Ps/(snr_val)) 
-    std2 = 50*std1
-
-        # #std1 = math.sqrt(0.02/(a0+50*a1))
-        # # print(Ps/(snr_val*(a0+50*a1)))
-
-        # printx("std1",std1)
-    
-# std1 = 0.1
-    # n1 = torch.randn(torch.tensor(list(global_dict.values())).shape) * std1        
-    n1 = torch.randn(global_dict.size())*std1
-    n2 = torch.randn(global_dict.size())*std2
-    
-    # n2 = torch.randn(torch.tensor(list(global_dict.values())).shape) * std2  
-    noise = a0*n1 + a1*n2
-
-    global_dict += noise
 
     global_model.load_state_dict(global_dict)
 
