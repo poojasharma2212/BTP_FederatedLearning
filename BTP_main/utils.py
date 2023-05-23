e952ec047b5ac75fda0a34ecee499da6ab492ddb
poojasharma2212 <56320354+poojasharma2212@users.noreply.github.com>
2023-05-23 14:51:20 +0530

    Update utils.py

--- BTP_main/utils.py
+++ BTP_main/utils.py
@@ -1,105 +1,105 @@
-import torch
-import math
+# import torch
+# import math
 
 
-def averageModels(global_model, clients, snr_value, Ps,alpha,K_clients,fed_round):
-    client_models = [clients[i]['model'] for i in range(len(clients))]
+# def averageModels(global_model, clients, snr_value, Ps,alpha,K_clients,fed_round):
+#     client_models = [clients[i]['model'] for i in range(len(clients))]
     
-    samples = [clients[i]['samples'] for i in range(len(clients))]
+#     samples = [clients[i]['samples'] for i in range(len(clients))]
 
-    global_dict = global_model.state_dict()
+#     global_dict = global_model.state_dict()
 
-    snr = snr_value
-    # print("SNR==", snr)
-    snr_val = 10**(snr/10)
-    std = math.sqrt(Ps/snr_val)
-    n1 = 0
-    n2 = 0
-    noise = 0
+#     snr = snr_value
+#     # print("SNR==", snr)
+#     snr_val = 10**(snr/10)
+#     std = math.sqrt(Ps/snr_val)
+#     n1 = 0
+#     n2 = 0
+#     noise = 0
 
-    std1 = math.sqrt(Ps/(snr_val)) 
-    std2 = 50*std1
-    # print(global_dict)
-    for k in global_dict.keys():  
-        client_weights = [client_models[i].state_dict()[k].float() * samples[i] for i in range(len(client_models))]
-        weighted_sum = torch.stack(client_weights, dim=0).sum(dim=0)
-        global_dict[k] = weighted_sum
+#     std1 = math.sqrt(Ps/(snr_val)) 
+#     std2 = 50*std1
+#     # print(global_dict)
+#     for k in global_dict.keys():  
+#         client_weights = [client_models[i].state_dict()[k].float() * samples[i] for i in range(len(client_models))]
+#         weighted_sum = torch.stack(client_weights, dim=0).sum(dim=0)
+#         global_dict[k] = weighted_sum
 
-        # Add Gaussian noise to the global model's parameters
-        # noise = torch.randn(global_dict[k].shape) * (std/(K_clients))
+#         # Add Gaussian noise to the global model's parameters
+#         # noise = torch.randn(global_dict[k].shape) * (std/(K_clients))
         
-        # noise = 0
+#         # noise = 0
             
-        if(fed_round == 5): #randomise round -- adding impulsive noise in random round
-            a0 = 0
-            a1 = 1
+#         if(fed_round == 5): #randomise round -- adding impulsive noise in random round
+#             a0 = 0
+#             a1 = 1
                 
-        else:
-            a0 = 1
-            a1 = 0
+#         else:
+#             a0 = 1
+#             a1 = 0
 
-        # print("Guassian value : ", a0)
+#         # print("Guassian value : ", a0)
 
 
-            # #std1 = math.sqrt(0.02/(a0+50*a1))
-            # # print(Ps/(snr_val*(a0+50*a1)))
+#             # #std1 = math.sqrt(0.02/(a0+50*a1))
+#             # # print(Ps/(snr_val*(a0+50*a1)))
 
-            # printx("std1",std1)
-    # std1 = 0.1
-        # n1 = torch.randn(torch.tensor(list(global_dict[k].values())).shape) 
-        # n1 = torch.randn(global_dict[k].shape)* std1        
-        # # n1 = torch.randn(global_dict.keys.size())*std1
-        # # n2 = torch.randn(global_dict.keys.size())*std2
+#             # printx("std1",std1)
+#     # std1 = 0.1
+#         # n1 = torch.randn(torch.tensor(list(global_dict[k].values())).shape) 
+#         # n1 = torch.randn(global_dict[k].shape)* std1        
+#         # # n1 = torch.randn(global_dict.keys.size())*std1
+#         # # n2 = torch.randn(global_dict.keys.size())*std2
         
-        # # n2 = torch.randn(torch.tensor(list(global_dict[k].values())).shape) 
-        # n2 = torch.randn(global_dict[k].shape) *std2
-        # noise = a0*n1 + a1*n2
+#         # # n2 = torch.randn(torch.tensor(list(global_dict[k].values())).shape) 
+#         # n2 = torch.randn(global_dict[k].shape) *std2
+#         # noise = a0*n1 + a1*n2
 
-        # global_dict += noise
-        print(noise.size())
-        # global_dict[k] += noise/(math.sqrt(K_clients))
+#         # global_dict += noise
+#         print(noise.size())
+#         # global_dict[k] += noise/(math.sqrt(K_clients))
 
-    global_model.load_state_dict(global_dict)
+#     global_model.load_state_dict(global_dict)
 
-    y_out = global_model.conv1.weight
-    # if(fed_round == 5): #randomise round -- adding impulsive noise in random round
-    #     a0 = 0
-    #     a1 = 1
+#     y_out = global_model.conv1.weight
+#     # if(fed_round == 5): #randomise round -- adding impulsive noise in random round
+#     #     a0 = 0
+#     #     a1 = 1
                 
-    # else:
-    #     a0 = 1
-    #     a1 = 0
+#     # else:
+#     #     a0 = 1
+#     #     a1 = 0
     
-    # n1 = torch.randn(y_out.shape)* std1        
-    # n2 = torch.randn(y_out.shape) *std2
-    # noise = a0*n1 + a1*n2
+#     # n1 = torch.randn(y_out.shape)* std1        
+#     # n2 = torch.randn(y_out.shape) *std2
+#     # noise = a0*n1 + a1*n2
 
-    # y_out = y_out + noise
+#     # y_out = y_out + noise
 
-    # print("noise")
-    # print(noise.size())
+#     # print("noise")
+#     # print(noise.size())
 
-    global_model.conv1.weight.data = y_out
+#     global_model.conv1.weight.data = y_out
 
-    y_out = global_model.conv2.weight
+#     y_out = global_model.conv2.weight
     
-    if(fed_round == 5): #randomise round -- adding impulsive noise in random round
-        a0 = 0
-        a1 = 1
+#     if(fed_round == 5): #randomise round -- adding impulsive noise in random round
+#         a0 = 0
+#         a1 = 1
                 
-    else:
-        a0 = 1
-        a1 = 0
+#     else:
+#         a0 = 1
+#         a1 = 0
     
-    n1 = torch.randn(y_out.shape)* std1        
-    n2 = torch.randn(y_out.shape) *std2
-    noise = a0*n1 + a1*n2
+#     n1 = torch.randn(y_out.shape)* std1        
+#     n2 = torch.randn(y_out.shape) *std2
+#     noise = a0*n1 + a1*n2
 
-    y_out = y_out + noise
+#     y_out = y_out + noise
 
-    print("noise")
-    print(noise.size())
+#     print("noise")
+#     print(noise.size())
 
-    global_model.conv2.weight.data = y_out
+#     global_model.conv2.weight.data = y_out
 
-    return global_model
+#     return global_model
